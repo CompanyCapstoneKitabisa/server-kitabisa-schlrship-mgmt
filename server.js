3701d2959a45f879ca65823613340b13e9616c24
Randy <randyhanjaya39@gmail.com>
2022-05-13 11:31:34 +0700

    edit server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req,res) => {res.send({message: "Hello world!"})})
+
 app.use('/users', userRoute);
 app.use('/login', loginRoute);
 
