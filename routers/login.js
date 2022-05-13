8db9e7dd943cded3689c4ea0d4fdca3f305e62c5
Randy <randyhanjaya39@gmail.com>
2022-05-13 12:17:50 +0700

    af

--- routers/login.js
+++ routers/login.js
@@ -1,51 +1,51 @@
-// const express = require('express');
-// const admin = require('firebase-admin');
-// const dbconf = require('./firebase conf.js');
-// const bcrypt = require('bcrypt');
+const express = require('express');
+const admin = require('firebase-admin');
+const dbconf = require('./firebase conf.js');
+const bcrypt = require('bcrypt');
 
-// const route = express();
+const route = express();
 
-// const db = dbconf.firestore();
+const db = dbconf.firestore();
 
-// route.post('/', (req,res) => {
-//     var usersRef = db.collection('users');
-//     const dataLogin = {
-//         email: req.body.email,
-//         password: req.body.password
-//     };
-//     var success = 0;
-//     let dataUserLogin = {};
+route.post('/', (req,res) => {
+    var usersRef = db.collection('users');
+    const dataLogin = {
+        email: req.body.email,
+        password: req.body.password
+    };
+    var success = 0;
+    let dataUserLogin = {};
 
-//     try{
-//         usersRef.get().then((data) => {
-//             data.forEach((userDoc) => {
-//                 var userDocData = userDoc.data()
-//                 if(userDocData.email === dataLogin.email && bcrypt.compareSync(dataLogin.password, userDocData.password)){
-//                     success = success + 1;
-//                     dataUserLogin = {
-//                         firstName: userDocData.firstName,
-//                         lastName: userDocData.lastName
-//                     }
-//                 }
-//             })
-//             if(success === 1){
-//                 res.status(200).send({
-//                     message: 'Berhasil login',
-//                     dataUserLogin
-//                 })
-//             }else if(success === 0){
-//                 res.status(404).send({
-//                     message: 'Gagal login'
-//                 })
-//             }
-//         })
-//     } catch (e) {
-//         res.status(500).send({message: 'Internal server error'})
-//     }
-// })
+    try{
+        usersRef.get().then((data) => {
+            data.forEach((userDoc) => {
+                var userDocData = userDoc.data()
+                if(userDocData.email === dataLogin.email && bcrypt.compareSync(dataLogin.password, userDocData.password)){
+                    success = success + 1;
+                    dataUserLogin = {
+                        firstName: userDocData.firstName,
+                        lastName: userDocData.lastName
+                    }
+                }
+            })
+            if(success === 1){
+                res.status(200).send({
+                    message: 'Berhasil login',
+                    dataUserLogin
+                })
+            }else if(success === 0){
+                res.status(404).send({
+                    message: 'Gagal login'
+                })
+            }
+        })
+    } catch (e) {
+        res.status(500).send({message: 'Internal server error'})
+    }
+})
 
-// route.get('/', (req,res) => {
-//     res.send({message: 'hallo'})
-// })
+route.get('/', (req,res) => {
+    res.send({message: 'hallo'})
+})
 
-// module.exports = route
+module.exports = route
